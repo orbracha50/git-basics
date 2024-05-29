e621bd2596c36ec2f5c021ade37a74532ad4deaa
Or Bracha <orbracha50@gmail.com>
2024-05-29 15:52:27 +0300

    Add ball6 style & func

--- main.js
+++ main.js
@@ -37,18 +37,32 @@ function onBallClick3(elball) {
     }
 }
 function onBallClick4(elball) {
-    ball1Size -= getRandomInt(20,61)
-    ball2Size -= getRandomInt(20,61)
-    if (ball1Size <100) ball1Size = 100
-    if (ball2Size <100) ball2Size = 100
+    ball1Size -= getRandomInt(20, 61)
+    ball2Size -= getRandomInt(20, 61)
+    if (ball1Size < 100) ball1Size = 100
+    if (ball2Size < 100) ball2Size = 100
     var elball1 = document.querySelector(`.ball1`)
     var elball2 = document.querySelector(`.ball2`)
     elball1.style.height = ball1Size + 'px'
-        elball1.style.width = ball1Size + 'px'
-        elball2.style.height = ball2Size + 'px'
-        elball2.style.width = ball2Size + 'px'
+    elball1.style.width = ball1Size + 'px'
+    elball2.style.height = ball2Size + 'px'
+    elball2.style.width = ball2Size + 'px'
 }
 function onBallClick5(elball) {
-    var elBody= document.querySelector(`body`)
+    var elBody = document.querySelector(`body`)
     elBody.style.backgroundColor = getRandomColor()
+}
+function onBallClick6(elball) {
+    ball1Size = 100
+    ball2Size = 150
+    var elball1 = document.querySelector(`.ball1`)
+    var elball2 = document.querySelector(`.ball2`)
+    var elBody = document.querySelector(`body`)
+    elball1.style.backgroundColor = 'blue'
+    elball2.style.backgroundColor = 'yellow'
+    elball1.style.height = ball1Size + 'px'
+    elball1.style.width = ball1Size + 'px'
+    elball2.style.height = ball2Size + 'px'
+    elball2.style.width = ball2Size + 'px'
+    elBody.style.backgroundColor = 'black'
 }
