b3137dd31b2a226934bb762940e2d0293b556b2a
Or Bracha <orbracha50@gmail.com>
2024-05-29 14:18:01 +0300

    Change Func BallClick

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 var ball1Size = 100
 function onBallClick(elball) {
     ball1Size += 50
+    if (ball1Size > 400) {
+        ball1Size = 100
+    }
     elball.style.height = ball1Size + 'px'
     elball.style.width = ball1Size + 'px'
+
 }
